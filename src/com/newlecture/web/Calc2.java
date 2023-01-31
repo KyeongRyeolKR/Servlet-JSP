8c69472313ed09be3068573ebb2380c1af698f5d
KyeongRyeol <rlarudfuf802@naver.com>
2023-02-01 02:30:51 +0900

    2023-02-01 / 02:30

--- src/com/newlecture/web/Calc2.java
+++ src/com/newlecture/web/Calc2.java
@@ -3,10 +3,7 @@ package com.newlecture.web;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -16,6 +13,8 @@ public class Calc2 extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletContext application = req.getSession().getServletContext();  // 어플리케이션 전역적인 저장소
         HttpSession session = req.getSession();     // 사용자(세션)마다 달라지는 저장소
+        Cookie[] cookies = req.getCookies();    // 사용자 브라우저에 저장할 수 있는 저장소
+
 
         resp.setContentType("text/html; charset=UTF-8");
         PrintWriter out = resp.getWriter();
@@ -31,11 +30,24 @@ public class Calc2 extends HttpServlet {
         // 계산
         if (op.equals("=")) {
 //            int x = (Integer) application.getAttribute("value");  // Object 타입으로 반환하기 때문에 (Integer)로 형변환
-            int x = (Integer) session.getAttribute("value");
+//            int x = (Integer) session.getAttribute("value");
+            int x = 0;
+            for(Cookie c : cookies){
+                if(c.getName().equals("value")){
+                    x = Integer.parseInt(c.getValue());
+                    break;
+                }
+            }
             int y = v;
 //            String operator = (String) application.getAttribute("op");
-            String operator = (String) session.getAttribute("op");
-
+//            String operator = (String) session.getAttribute("op");
+            String operator = "";
+            for(Cookie c : cookies){
+                if(c.getName().equals("op")){
+                    operator = c.getValue();
+                    break;
+                }
+            }
             int result = 0;
 
             if (operator.equals("+")) {
@@ -46,9 +58,18 @@ public class Calc2 extends HttpServlet {
             out.println("<br/>결과 : " + result);
         } else {    // 값 저장
 //            application.setAttribute("value", v);
-            session.setAttribute("value", v);
 //            application.setAttribute("op", op);
-            session.setAttribute("op", op);
+//            session.setAttribute("value", v);
+//            session.setAttribute("op", op);
+            Cookie valueCookie = new Cookie("value", String.valueOf(v));  // 쿠키는 문자열로만 저장해야함
+            Cookie opCookie = new Cookie("op", op);
+            valueCookie.setPath("/calc2");   // 쿠키를 저장할 경로를 지정해줌
+            valueCookie.setMaxAge(24*60*60);    // 쿠키의 만료 날짜 설정(초단위)
+            opCookie.setPath("/calc2");
+            resp.addCookie(valueCookie);
+            resp.addCookie(opCookie);
+
+            resp.sendRedirect("calc2.html");  // 값 저장이 끝난 후 calc2.html로 리디렉트
         }
 
     }
